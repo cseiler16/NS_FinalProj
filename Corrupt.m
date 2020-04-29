cdfd4b6613d6b5fa3ffeca4a4c9d986d9f29a553
morganliam <morganliam@ufl.edu>
2020-04-28 20:15:20 -0400

    Update Corrupt.m

--- Corrupt.m
+++ Corrupt.m
@@ -1,7 +1,8 @@
 function result_array = corrupt(x)
-BERCalc
-close all;
-BER = berOQPSK2450(10);
+%BERCalc
+%close all;
+%BER = berOQPSK2450(10);
+BER = 0.01;
 len = length(x);
 BER_losses = ceil(BER*len);
 i = randperm(len,BER_losses);
